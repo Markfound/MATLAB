1c9918b74da285b8d1f69af8051317c3cfe5e211
Markfound <739851970@qq.com>
2019-06-19 23:50:23 +0800

    nothing

--- audiopro/audiopro2.m
+++
@@ -1,7 +0,0 @@
-clear all;
-clc;
-[Myaudio Fs]=audioread('Myvoice.mp3');
-x=0:0.1:60000;
-y=Myaudio(10000:610000,1)';
-
-plot(x,y);
--- audiopro/audiopro3.m
+++
@@ -1,4 +0,0 @@
-tem96k=audioread('96K.mp3');
-tem192k=audioread('192K.mp3');
-size(tem96k)
-size(tem192k)
